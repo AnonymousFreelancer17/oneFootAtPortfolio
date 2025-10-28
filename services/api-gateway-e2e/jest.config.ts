6471afaf2f947a9786e117a6d3fcc8145a99c216
AnonymousFreelancer17 <anonymousfreelancer17@gmail.com>
2025-10-28 15:50:54 +0530

    Initial Commit

--- services/api-gateway-e2e/jest.config.ts
+++ services/api-gateway-e2e/jest.config.ts
@@ -10,12 +10,15 @@ const swcJestConfig = JSON.parse(
 swcJestConfig.swcrc = false;
 
 export default {
-  displayName: '@one-foot-at-portfolio/auth-services',
+  displayName: '@one-foot-at-portfolio/api-gateway-e2e',
   preset: '../../jest.preset.js',
+  globalSetup: '<rootDir>/src/support/global-setup.ts',
+  globalTeardown: '<rootDir>/src/support/global-teardown.ts',
+  setupFiles: ['<rootDir>/src/support/test-setup.ts'],
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['@swc/jest', swcJestConfig]
+    '^.+\\.[tj]s$': ['@swc/jest', swcJestConfig],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: 'test-output/jest/coverage'
+  coverageDirectory: 'test-output/jest/coverage',
 };
