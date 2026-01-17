18849a3c1b33053da2bab2ac3804e5a2d54465c7
AnonymousFreelancer17 <anonymousfreelancer17@gmail.com>
2026-01-17 18:56:04 +0530

    Fixed: TsConfig

---
+++ apps/restaurant/src/app/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='w-screen h-screen bg-red-400 flex justify-center items-center'>Home</div>
+  )
+}
+
+export default Home
--- apps/restaurant/src/app/app.tsx
+++ apps/restaurant/src/app/app.tsx
@@ -1,43 +1,19 @@
+import Home from "./Home";
 import NxWelcome from "./nx-welcome";
 
-import { Route, Routes, Link } from 'react-router-dom';
+//  navbar
+// import {Navbar} from "@ui" 
+
+
+import { Route, Routes, Link, BrowserRouter, Router } from "react-router-dom";
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="@one-foot-at-portfolio/restraunt"/>
-    
-    {/* START: routes */}
-    {/* These routes and navigation have been generated for you */}
-    {/* Feel free to move and update them to fit your needs */}
-    <br/>
-    <hr/>
-    <br/>
-    <div role="navigation">
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/page-2">Page 2</Link></li>
-      </ul>
-    </div>
     <Routes>
-      <Route
-        path="/"
-        element={
-          <div>This is the generated root route. <Link to="/page-2">Click here for page 2.</Link></div>
-        }
-      />
-      <Route
-        path="/page-2"
-        element={
-          <div><Link to="/">Click here to go back to root page.</Link></div>
-        }
-      />
+      {/* <Navbar /> */}
+      <Route path="/" element={<Home />} />
     </Routes>
-    {/* END: routes */}
-    </div>
   );
 }
 
 export default App;
-
-
